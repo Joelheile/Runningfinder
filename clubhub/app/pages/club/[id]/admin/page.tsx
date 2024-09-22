e00a09f33eab9f7ece5164c1384ac186b9e8ed64
Joelheile <joel.heil-escobar@outlook.de>
2024-09-22 20:54:11 +0200

    fix: changed url structure

--- clubhub/app/pages/club/[id]/admin/page.tsx
+++ clubhub/app/pages/club/[id]/admin/page.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 
-const EditClubPage = () => {
+const AdminPage = () => {
   return (
     <>
 
@@ -9,4 +9,4 @@ const EditClubPage = () => {
   );
 };
 
-export default EditClubPage;
+export default AdminPage;
