51d933b9d311d8e2855f52908f62eb95c1c2f02a
Joelheile <joel.heil-escobar@outlook.de>
2024-11-08 08:33:50 +0100

    🛂 Working auth routes

--- clubhub/src/lib/authentication/auth.ts
+++ clubhub/src/lib/authentication/auth.ts
@@ -1,28 +1,5 @@
-import NextAuth from 'next-auth'
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
-
-
-
-
-
-import { DrizzleAdapter } from '@auth/drizzle-adapter'
-import { db } from '../db/db'
-import { accounts, sessions, user, verificationTokens } from '../db/schema'
-import Resend from "next-auth/providers/resend"
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db, {
-    usersTable: user,
-    accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
-  }),
-  providers: [
-    //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
-   Resend({
-    from: "team@runningfinder.de",
-   })
-  ],
- 
-  experimental: { enableWebAuthn: true },    
-  })
+export const { handlers, auth } = 
+NextAuth({ providers: [GitHub] });
