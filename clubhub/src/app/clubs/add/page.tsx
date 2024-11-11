c84560a1ff46e2df27cd82977e63488579d38804
Joelheile <joel.heil-escobar@outlook.de>
2024-11-11 17:30:08 +0100

    💄 centered club add component

--- clubhub/src/app/clubs/add/page.tsx
+++ clubhub/src/app/clubs/add/page.tsx
@@ -3,9 +3,9 @@ import AddRun from "@/components/runs/AddRun";
 
 export default function addClubPage() {
   return (
-    <div className="flex-col p-10  items-center w-2/3">
-      <h1 className="text-2xl font-bold mb-4">Add Club</h1>
-      <div className="flex justify-between">
+    <div className="flex-col p-10 items-center w-2/3 mx-auto">
+      <h1 className="text-2xl font-bold mb-4 text-center">Add Club</h1>
+      <div className="flex justify-center">
         <AddClub />
       </div>
     </div>
