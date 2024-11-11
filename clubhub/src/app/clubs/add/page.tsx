4d2eda1d0d613c9823e67d967699d90951bb55b5
Joelheile <joel.heil-escobar@outlook.de>
2024-11-11 16:14:18 +0100

    🚚 renamed club and run to plural and moved addRun to own path

---
+++ clubhub/src/app/clubs/add/page.tsx
@@ -0,0 +1,13 @@
+import AddClub from "@/components/clubs/AddClub";
+import AddRun from "@/components/runs/AddRun";
+
+export default function addClubPage() {
+  return (
+    <div className="flex-col p-10  items-center w-2/3">
+      <h1 className="text-2xl font-bold mb-4">Add Club</h1>
+      <div className="flex justify-between">
+        <AddClub />
+      </div>
+    </div>
+  );
+}
