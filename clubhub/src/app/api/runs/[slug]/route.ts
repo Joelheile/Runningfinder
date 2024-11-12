9fbde5790794859b083eff3f23e5ade58fe6c065
Joelheile <joel.heil-escobar@outlook.de>
2024-11-12 15:30:29 +0100

    🚚 renamed path from clubId to slug

---
+++ clubhub/src/app/api/runs/[slug]/route.ts
@@ -0,0 +1,24 @@
+import { db } from "@/lib/db/db";
+import { runs } from "@/lib/db/schema/runs";
+import { clubs } from "@/lib/db/schema/clubs";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function GET({clubId}: {clubId: string}) {
+
+  try {
+    const res = await db
+      .select()
+      .from(runs)
+      .leftJoin(clubs, eq(clubs.id, runs.clubId))
+      .execute();
+
+    return NextResponse.json(res);
+  } catch (error) {
+    console.error("Error fetching runs with clubs:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
