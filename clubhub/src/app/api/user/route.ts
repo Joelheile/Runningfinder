54f969e659dfcc3bc5495720286080443b233bf7
Joelheile <joel.heil-escobar@outlook.de>
2024-11-12 19:03:41 +0100

    🏗️ added user route

---
+++ clubhub/src/app/api/user/route.ts
@@ -0,0 +1,18 @@
+import { db } from "@/lib/db/db";
+import { users } from "@/lib/db/schema/users";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function GET({userId}: {userId: string}) {
+    try {
+      const res = await db.select().from(users).where(eq(users.id, userId) );
+  
+      return NextResponse.json(res);
+    } catch (error) {
+      console.error("Error fetching registrations:", error);
+      return NextResponse.json(
+        { error: "Internal Server Error" },
+        { status: 500 },
+      );
+    }
+  }
