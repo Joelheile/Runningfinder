4c9b6a3f77984a48f807890f8738ac1926856215
Joelheile <joel.heil-escobar@outlook.de>
2024-11-12 19:03:58 +0100

    ♻️ refactored add run into two components

--- clubhub/src/components/runs/AddRun.tsx
+++ clubhub/src/components/runs/AddRun.tsx
@@ -1,21 +1,13 @@
-import { useEffect, useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { MapLocationPicker } from "../Map/MapLocationPicker";
-import { Run } from "@/lib/types/Run";
-import { useAddRun } from "@/lib/hooks/useAddRun";
-import { Button } from "../ui/button";
-
-import { weekdays } from "@/lib/weekdays";
-
+import { useState, useEffect } from "react";
 import { v4 } from "uuid";
+import { useAddRun } from "@/lib/hooks/useAddRun";
+import { Run } from "@/lib/types/Run";
 import { Club } from "@/lib/types/Club";
 import toast from "react-hot-toast";
-import { useSession } from "next-auth/react";
-import { auth } from "@/lib/authentication/auth";
+import { weekdays } from "@/lib/weekdays";
+import AddRunUI from "./AddRunUI";
 
-export default function AddRun({ club }: { club: Club }) {
+export default function AddRunState({ club }: { club: Club }) {
   const [name, setName] = useState("");
   const [difficulty, setDifficulty] = useState("easy");
   const [startDescription, setStartDescription] = useState("");
@@ -25,13 +17,16 @@ export default function AddRun({ club }: { club: Club }) {
   const [location, setLocation] = useState({ lat: 52.52, lng: 13.405 });
   const [membersOnly, setMembersOnly] = useState(false);
   const [interval, setInterval] = useState("weekly");
+
+  const mutation = useAddRun();
+
   const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     if (!name || !difficulty || !startTime || !distance) {
       toast("You are missing some important fields!", {
         icon: "🔎",
       });
     } else {
-      e.preventDefault();
       const formData: Run = {
         name,
         difficulty,
@@ -53,8 +48,6 @@ export default function AddRun({ club }: { club: Club }) {
     }
   };
 
-  const mutation = useAddRun();
-
   const handleSelect = (lat: number, lng: number) => {
     console.log("Selected location:", lat, lng);
     setLocation({ lat, lng });
@@ -64,108 +57,29 @@ export default function AddRun({ club }: { club: Club }) {
     console.log("Location updated:", location);
     toast.success(`${location.lat} ${location.lng}`);
   }, [location]);
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="grid gap-4">
-          <div className="flex flex-col">
-            <Label>Name</Label>
-            <Input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-          <div className="flex flex-col">
-            <Label>Difficulty</Label>
-            <select
-              value={difficulty}
-              onChange={(e) => setDifficulty(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            >
-              <option value="easy">Easy</option>
-              <option value="intermediate">Intermediate</option>
-              <option value="advanced">Advanced</option>
-            </select>
-          </div>
-          <div className="flex flex-col">
-            <Label>Interval</Label>
-            <select
-              value={interval}
-              onChange={(e) => setInterval(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            >
-              <option value="daily">Daily</option>
-              <option value="weekly">Weekly</option>
-              <option value="biweekly">Biweekly</option>
-              <option value="monthly">Monthly</option>
-            </select>
-          </div>
-          <div className="flex flex-col">
-            <Label>Weekday</Label>
-            <select
-              value={weekday}
-              onChange={(e) => setWeekday(Number(e.target.value))}
-              className="mt-1 p-2 border rounded"
-            >
-              {weekdays.map((day) => (
-                <option key={day.value} value={day.value}>
-                  {day.name}
-                </option>
-              ))}
-            </select>
-          </div>
 
-          <div className="flex flex-col">
-            <Label>Start Time</Label>
-            <Input
-              type="time"
-              value={startTime}
-              onChange={(e) => setStartTime(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-          <div className="flex flex-col">
-            <Label>Distance (km)</Label>
-            <Input
-              type="number"
-              value={distance}
-              onChange={(e) => setDistance(Number(e.target.value))}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-          <div className="flex flex-col">
-            <Label>Describe the meeting point</Label>
-            <Input
-              value={startDescription}
-              onChange={(e) => setStartDescription(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-          <div className="flex items-center space-x-2">
-            <Label>MembersOnly</Label>
-            <input
-              type="checkbox"
-              checked={membersOnly}
-              onChange={() => setMembersOnly(!membersOnly)}
-              className="mt-1   p-2 border rounded"
-            />
-          </div>
-        </div>
+  const runProps = {
+    name,
+    setName,
+    difficulty,
+    setDifficulty,
+    startDescription,
+    setStartDescription,
+    weekday,
+    setWeekday,
+    startTime,
+    setStartTime,
+    distance,
+    setDistance,
+    location,
+    setLocation,
+    membersOnly,
+    setMembersOnly,
+    interval,
+    setInterval,
+    handleSubmit,
+    handleSelect,
+  };
 
-        <Button
-          type="submit"
-          className="mt-4 p-2 bg-blue-500 text-white rounded"
-        >
-          Add run
-        </Button>
-        <div className="App mt-8"></div>
-        <MapLocationPicker
-          onSelect={handleSelect}
-          onCancel={() => {}}
-          location={location}
-        />
-      </form>
-    </div>
-  );
+  return <AddRunUI {...runProps} />;
 }
--- clubhub/src/components/runs/AddRunUI.tsx
+++ clubhub/src/components/runs/AddRunUI.tsx
@@ -1,4 +1,3 @@
-// AddRunUI.tsx
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "../ui/button";
