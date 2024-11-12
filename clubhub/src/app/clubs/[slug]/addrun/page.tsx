c121b2e865506f2812d5f0737f06d2afccff6fc1
Joelheile <joel.heil-escobar@outlook.de>
2024-11-12 19:04:22 +0100

    ♻️ refactored add run

--- clubhub/src/app/clubs/[slug]/addrun/page.tsx
+++ clubhub/src/app/clubs/[slug]/addrun/page.tsx
@@ -1,15 +1,10 @@
 "use client";
-import AddClub from "@/components/clubs/AddClub";
 import AddRun from "@/components/runs/AddRun";
-import { useState } from "react";
-import { Run } from "@/lib/types/Run";
 import { useParams } from "next/navigation";
-import { useFetchClubBySlug, useFetchClubs } from "@/lib/hooks/useFetchClubs";
-import { Skeleton } from "@/components/ui/skeleton";
-import { auth } from "@/lib/authentication/auth";
+import { useFetchClubBySlug } from "@/lib/hooks/useFetchClubs";
 import AddRunSkeleton from "@/components/runs/addRunSkeleton";
 
-export default  function AddRunsPage() {
+export default function AddRunsPage() {
   const slug = useParams().slug.toString();
 
   const {
@@ -18,28 +13,19 @@ export default  function AddRunsPage() {
     error: clubsError,
   } = useFetchClubBySlug(slug);
 
+  if (clubsLoading) {
+    return <AddRunSkeleton />;
+  }
+
+  if (clubsError || !club) {
+    return <p>Club could not be found</p>;
+  }
+
   return (
     <div className="flex-col p-10 items-center w-2/3 mx-auto">
-      {club && (
-        <h1 className="text-2xl font-bold mb-4">Add Runs to "{club?.name}"</h1>
-      )}
-
+      <h1 className="text-2xl font-bold mb-4">Add Runs to "{club.name}"</h1>
       <div className="flex flex-col gap-6 w-full">
-        {club && (
-          <>
-            <AddRun club={club} />
-          </>
-        )}
-        {clubsError && (
-          <>
-            <p>Club could not be found</p>
-          </>
-        )}
-        {clubsLoading && (
-          <>
-           <AddRunSkeleton/>
-          </>
-        )}
+        <AddRun club={club} />
       </div>
     </div>
   );
