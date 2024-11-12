fe63dc27df60a80aa8197530d1180d1fd8c89438
Joelheile <joel.heil-escobar@outlook.de>
2024-11-12 11:08:28 +0100

    🔐 Changed db secret name to next_public

--- clubhub/src/lib/db/db.ts
+++ clubhub/src/lib/db/db.ts
@@ -2,5 +2,5 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 config({ path: ".env.local" });
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL!);
 export const db = drizzle(sql);
