48892c1799f01a4cae9ad3177ae360b457261985
Joelheile <joel.heil-escobar@outlook.de>
2024-11-12 14:03:07 +0100

    📱Responsiveness UI fixes

--- clubhub/src/components/runs/FilterBarUI.tsx
+++ clubhub/src/components/runs/FilterBarUI.tsx
@@ -16,7 +16,7 @@ interface FilterBarUIProps {
   setDistanceIndex: (index: number | null) => void;
   difficulty: string | null;
   setDifficulty: (value: string | null) => void;
-  resetFilters: () => void; 
+  resetFilters: () => void;
 }
 
 export default function FilterBarUI({
@@ -33,87 +33,96 @@ export default function FilterBarUI({
 
   return (
     <div className="bg-white/90 backdrop-blur-sm absolute bottom-0 left-0 z-10 w-full text-card-foreground shadow-lg p-6 space-y-6">
-      <div className="flex flex-col space-y-4">
-        <div className="space-y-4 flex flex-col w-1/5 ">
-          <label className="font-semibold text-lg">When are you free?</label>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button variant="outline" className=" sm:w-auto">
-                Select Days
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-full sm:w-auto p-4">
-              <div className="space-y-2">
-                {weekdays.map((day) => (
-                  <div key={day.value} className="flex items-center space-x-2">
-                    <Checkbox
-                      id={day.name}
-                      checked={selectedDays.includes(day.value)}
-                      onCheckedChange={() => toggleDay(day.value)}
-                    />
-                    <label
-                      htmlFor={day.name}
-                      className="font-medium leading-none"
+      <div className=" flex-col space-y-4">
+        <div className="flex flex-row gap-x-5 w-auto ">
+          <div className="space-y-4 flex flex-col w-1/3">
+            <label className="font-semibold text-lg">When are you free?</label>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline" className=" sm:w-auto">
+                  Select Days
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className=" p-4">
+                <div className="space-y-2">
+                  {weekdays.map((day) => (
+                    <div
+                      key={day.value}
+                      className="flex items-center space-x-2"
                     >
-                      {day.name}
-                    </label>
-                  </div>
+                      <Checkbox
+                        id={day.name}
+                        checked={selectedDays.includes(day.value)}
+                        onCheckedChange={() => toggleDay(day.value)}
+                      />
+                      <label
+                        htmlFor={day.name}
+                        className="font-medium leading-none"
+                      >
+                        {day.name}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="space-y-4 w-2/3">
+            <label className="font-semibold text-lg">
+              Your preferred distance (km)
+            </label>
+            <div className="p-4 border border-input bg-background hover:bg-accent hover:text-accent-foreground rounded-md">
+              <Slider
+                id="distance-filter"
+                min={0}
+                max={allowedDistances.length - 1}
+                step={1}
+                value={
+                  distanceIndex !== null
+                    ? [distanceIndex]
+                    : [allowedDistances.length - 1]
+                }
+                onValueChange={(value: number[]) => setDistanceIndex(value[0])}
+                className="w-full"
+              />
+              <div className="flex mt-2 justify-between text-sm">
+                {allowedDistances.map((value) => (
+                  <span key={value}>{value}</span>
                 ))}
               </div>
-            </PopoverContent>
-          </Popover>
-        </div>
-        <div className="space-y-4">
-          <label className="font-semibold text-lg">
-            Your preferred distance (km)
-          </label>
-          <div className="p-4 border border-input bg-background hover:bg-accent hover:text-accent-foreground rounded-md">
-            <Slider
-              id="distance-filter"
-              min={0}
-              max={allowedDistances.length - 1}
-              step={1}
-              value={
-                distanceIndex !== null
-                  ? [distanceIndex]
-                  : [allowedDistances.length - 1]
-              }
-              onValueChange={(value: number[]) => setDistanceIndex(value[0])}
-              className="w-full"
-            />
-            <div className="flex mt-2 justify-between text-sm">
-              {allowedDistances.map((value) => (
-                <span key={value}>{value}</span>
-              ))}
             </div>
           </div>
         </div>
         <div className="space-y-4">
           <label className="font-semibold text-lg">Select Difficulty</label>
-          <div className="flex space-x-4">
-            {difficulties.map((level) => (
-              <button
-                key={level}
-                className={`py-2 px-4 rounded ${
-                  difficulty === level
-                    ? "bg-blue-500 text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() =>
-                  setDifficulty(difficulty === level ? null : level)
-                }
+          <div className="flex flex-col md:flex-row justify-between">
+            <div className="flex space-x-4 mb-2 md:mb-0">
+              {difficulties.map((level) => (
+                <button
+                  key={level}
+                  className={`py-2 px-4 rounded w-full md:w-auto ${
+                    difficulty === level
+                      ? "bg-blue-500 text-white"
+                      : "bg-gray-200"
+                  }`}
+                  onClick={() =>
+                    setDifficulty(difficulty === level ? null : level)
+                  }
+                >
+                  {level.charAt(0).toUpperCase() + level.slice(1)}
+                </button>
+              ))}
+            </div>
+            <div>
+              <Button
+                variant="outline"
+                onClick={resetFilters}
+                className="w-full md:w-auto"
               >
-                {level.charAt(0).toUpperCase() + level.slice(1)}
-              </button>
-            ))}
+                Reset Filters
+              </Button>
+            </div>
           </div>
-        </div>
-
-//TODO: make it responsive
-        <div>
-          <Button variant="outline" onClick={resetFilters}>
-            Reset Filters
-          </Button>
         </div>
       </div>
     </div>
