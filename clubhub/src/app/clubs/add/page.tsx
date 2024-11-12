418a9ee1fc2d43439131019e1531b0b8f80986b2
Joelheile <joel.heil-escobar@outlook.de>
2024-11-12 15:51:21 +0100

    🛂 Implemented protected route for adding club path

--- clubhub/src/app/clubs/add/page.tsx
+++ clubhub/src/app/clubs/add/page.tsx
@@ -1,12 +1,14 @@
 import AddClub from "@/components/clubs/AddClub";
 import AddRun from "@/components/runs/AddRun";
+import { auth } from "@/lib/authentication/auth";
+import { redirect } from "next/navigation";
 
 export default async function addClubPage() {
- const session = auth
+  const session = await auth();
 
-  const session = await console.log("session", session);
+  console.log("session", session);
   if (!session?.user) {
-    redirect("/apu/auth/signin?callbackUrl=/clubs/add");
+    redirect("/api/auth/signin?callbackUrl=/clubs/add");
   }
   return (
     <div className="flex-col p-10 items-center w-2/3 mx-auto">
