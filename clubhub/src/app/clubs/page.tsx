d9e471e78e4943af44126bde3ffe87046f6e7120
Joelheile <joel.heil-escobar@outlook.de>
2024-11-13 10:17:03 +0100

    ✨ Implemented overview for all clubs

---
+++ clubhub/src/app/clubs/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import ClubCard from "@/components/Clubs/ClubCard";
+import { useFetchClubs } from "@/lib/hooks/useFetchClubs";
+import Link from "next/link";
+
+export default function ClubsDashboard() {
+  const { data: clubs, isLoading, isError, error } = useFetchClubs();
+
+  return (
+    <div className="flex flex-col items-center p-5">
+      <h1 className="text-2xl font-bold mt-5">Ready for your next run?</h1>
+      <p className="text-center mb-2">Discover and connect with clubs that vibe with you</p>
+      <div className="grid grid-cols-3 gap-x-5 p-5">
+        {clubs?.map((club) => (
+          <Link key={club.id} href={`/clubs/${club.slug}`}>
+            <ClubCard
+              avatarUrl={club.avatarUrl}
+              name={club.name}
+              description={club.description}
+              instagramUsername={club.instagramUsername}
+              websiteUrl={club.websiteUrl}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
--- clubhub/src/components/Clubs/ClubCard.tsx
+++ clubhub/src/components/Clubs/ClubCard.tsx
@@ -1,7 +1,7 @@
 import ClubIconBar from "../icons/ClubIconBar";
 
 interface ClubCardProps {
-  avatarUrl: string;
+  avatarUrl?: string;
   name: string;
   description: string;
   instagramUsername: string;
@@ -16,17 +16,17 @@ export default function ClubCard({
   websiteUrl,
 }: ClubCardProps) {
   return (
-    <div className="flex-col mt-5 lg:max-w-7xl md:w-2/3  sm:flex-row justify-center self-center">
-      <div className="flex flex-col sm:flex-row  bg-white gap-x-5 border p-3 rounded-md">
+    <div className="flex flex-col mt-5 max-w-full   sm:flex-row justify-center self-center">
+      <div className="flex flex-col sm:flex-row bg-white gap-x-5 border p-3 rounded-md ">
         <img
-          src={avatarUrl}
+          src={avatarUrl || "/assets/default-fallback-image.png"}
           alt={name}
-          className="rounded-md border lg:w-1/4 sm:w-1/6 h-auto max-h-48 object-cover mb-4 sm:mb-0 sm:mr-6"
+          className="rounded-md border w-full sm:w-1/3 lg:w-1/5 h-auto object-cover mb-4 sm:mb-0 sm:mr-6"
         />
-        <div className="flex flex-col max-w-2xl py-4 justify-between">
+        <div className="flex flex-col justify-between">
           <div>
-            <h1>{name}</h1>
-            <p className="mt-2">{description}</p>
+            <h1 className="text-xl font-semibold">{name}</h1>
+            <p className="mt-2 text-gray-600">{description}</p>
           </div>
           <ClubIconBar
             instagramUsername={instagramUsername}
