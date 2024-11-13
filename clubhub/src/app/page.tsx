31adb4c9fc686b42158397b073ef4fb9d0c8de39
Joelheile <joel.heil-escobar@outlook.de>
2024-11-13 09:34:56 +0100

    ✨Added Button to club dashboard

--- clubhub/src/app/page.tsx
+++ clubhub/src/app/page.tsx
@@ -1,5 +1,7 @@
 import MapPage from "@/components/Map/MapPage";
+import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/authentication/auth";
+import Link from "next/link";
 
 export default async function HomePage() {
   const session = await auth();
@@ -7,6 +9,12 @@ export default async function HomePage() {
 
   return (
     <div>
+      <div className="absolute z-10 top-2 right-1/2 left-1/2   text-center">
+        <Link href="/clubs">
+          <Button variant={"outline"}>All Clubs 🏃</Button>
+        </Link>
+      </div>
+
       <MapPage session={session} />
     </div>
   );
