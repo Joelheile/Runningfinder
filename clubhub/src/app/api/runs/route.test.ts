9ebd31f29d55e44da1381af05255a0deaa2698ef
Joelheile <joel.heil-escobar@outlook.de>
2024-11-13 11:50:50 +0100

    ✅ Passing test for getting and creating a run

--- clubhub/src/app/api/runs/route.test.ts
+++ clubhub/src/app/api/runs/route.test.ts
@@ -1,14 +1,35 @@
-/**
- * @jest-environment node
- */
-
 import { v4 } from "uuid";
-import {  GET, POST } from "./route";
+import { GET, POST } from "./route";
 
-it("should return data with status 200", async () => {
+let clubId: string;
+
+describe("API Routes", () => {
+  it("should return data with status 200", async () => {
     const request = new Request("https://localhost:3000/api/runs");
-  const response = await GET(request);
-  const body = await response.json();
-  console.log("response", body);
-  expect(response.status).toBe(200);
+    const response = await GET(request);
+    const body = await response.json();
+    console.log("response", body);
+    expect(response.status).toBe(200);
+
+    clubId = body[0].clubId; 
+    console.log("clubId", clubId);
+  });
+
+  it("should return added data with status 200", async () => {
+    const requestObj = {
+      json: async () => ({
+        id: v4(),
+        clubId: clubId,
+        name: "testrun",
+        distance: 5,
+        time: "01:01",
+        difficulty: "easy",
+        location: { lng: 0, lat: 0 },
+      }),
+    } as any;
+
+    const response = await POST(requestObj);
+
+    expect(response.status).toBe(201);
+  });
 });
