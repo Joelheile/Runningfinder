a8b2fe8fad7bbd523896d051ed241de759c0e923
Joelheile <joel.heil-escobar@outlook.de>
2024-11-14 10:31:35 +0100

    ♻️ Refactored Selected Club Header to logic and ui component

--- clubhub/src/components/Clubs/SelectedClubHeaderUI.tsx
+++ clubhub/src/components/Clubs/SelectedClubHeaderUI.tsx
@@ -1,51 +1,41 @@
-import { useFetchClubs } from "@/lib/hooks/clubs/useFetchClubs";
-import { Run } from "@/lib/types/Run";
+import { Club } from "@/lib/types/Club";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
 import ClubIconBar from "../Icons/ClubIconBar";
 
-import ClubHeaderSkeleton from "./ClubHeaderSkeleton";
+interface SelectedClubHeaderUIProps {
+  club: Club;
+  avatarUrl: string;
+  instagramSelected: boolean;
+  setInstagramSelected: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-export default function SelectedClubHeader({ run }: { run: Run }) {
-  const { data, error, isLoading } = useFetchClubs();
-
-  const club = data?.find((club) => club.id === run?.clubId);
-
-  if (!club) {
-    return <div>Club not found</div>;
-  }
-
-  if (isLoading) {
-    return <ClubHeaderSkeleton />;
-  }
-
-  const avatarUrl = club?.avatarUrl
-    ? club.avatarUrl
-    : "/assets/default-fallback-image.png";
-
-  const [instagramSelected, setInstagramSelected] = useState(false);
-
+export default function SelectedClubHeaderUI({
+  club,
+  avatarUrl,
+  instagramSelected,
+  setInstagramSelected,
+}: SelectedClubHeaderUIProps) {
   return (
     <div className="bg-white/80 backdrop-blur-sm absolute top-0 left-0 z-10 w-full text-card-foreground shadow-sm lg:p-6 sm:p-4 space-y-4">
-      <Link href={`/clubs/${club?.slug}`}>
+      <Link href={`/clubs/${club.slug}`}>
         <div className="flex flex-col sm:flex-row justify-between items-start">
           <div className="flex flex-col sm:flex-row w-full">
             <img
               src={avatarUrl}
-              alt={club?.name}
+              alt={club.name}
               className="rounded-md border w-auto max-w-48 sm:w-1/6 h-auto max-h-48 object-cover mb-4 sm:mb-0 sm:mr-6"
             />
             <div className="flex flex-col justify-between w-full sm:w-2/3">
               <div>
                 <h2 className="text-lg sm:text-xl font-semibold">
-                  {club?.name}
+                  {club.name}
                 </h2>
-                <p className="lg:w-1/3 mt-2">{club?.description}</p>
+                <p className="lg:w-1/3 mt-2">{club.description}</p>
               </div>
               <ClubIconBar
-                instagramUsername={club?.instagramUsername || ""}
-                websiteUrl={club?.websiteUrl || ""}
+                instagramUsername={club.instagramUsername || ""}
+                websiteUrl={club.websiteUrl || ""}
               />
             </div>
           </div>
