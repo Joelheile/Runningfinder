09cd7684dca7b241b01942b81ddee2c3afa6f501
Joelheile <joel.heil-escobar@outlook.de>
2024-11-14 17:42:09 +0100

    ♻️ separate UI from logic

--- src/components/Clubs/AddClubLogic.tsx
+++ src/components/Clubs/AddClubLogic.tsx
@@ -64,55 +64,6 @@ export default function AddClub() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="grid gap-4">
-          <div className="flex flex-col">
-            <Label>Club Name</Label>
-            <Input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-          <div className="flex flex-col">
-            <Label>Club Description</Label>
-            <Textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-          <div className="flex flex-col">
-            <Label>Website Url (https://)</Label>
-            <Input
-              value={websiteUrl}
-              onChange={(e) => setWebsiteUrl(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-          <div className="flex flex-col">
-            <Label>Instagram Username</Label>
-            <Input
-              value={instagramUsername}
-              onChange={(e) => setInstagramUsername(e.target.value)}
-              className="mt-1 p-2 border rounded"
-            />
-          </div>
-
-          <AvatarUploader
-            id={avatarFileId}
-            onUploadChange={handleUploadChange}
-          />
-          <Button
-            type="submit"
-            className="mt-4 p-2 bg-blue-500 text-white rounded"
-          >
-            Add club
-          </Button>
-        </div>
-        <div className="App mt-8"></div>
-      </form>
-    </div>
+    
   );
 }
