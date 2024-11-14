d9edefc4134b0b133cf9e4d478c91720332318d1
Joelheile <joel.heil-escobar@outlook.de>
2024-11-14 13:45:23 +0100

    🏗️ changed request header and swagger docu

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,20 @@
 import { db } from "@/lib/db/db";
 import { users } from "@/lib/db/schema/users";
 import { eq } from "drizzle-orm";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ userId }: { userId: string }) {
+export async function GET(request: NextRequest) {
   try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User ID is required" },
+        { status: 400 },
+      );
+    }
+
     const res = await db.select().from(users).where(eq(users.id, userId));
 
     return NextResponse.json(res);
@@ -25,7 +35,7 @@ export async function GET({ userId }: { userId: string }) {
  *     tags:
  *       - users
  *     parameters:
- *       - in: path
+ *       - in: query
  *         name: userId
  *         required: true
  *         schema:
@@ -35,6 +45,8 @@ export async function GET({ userId }: { userId: string }) {
  *         description: Returns the user data.
  *         content:
  *           application/json:
+ *       400:
+ *         description: User ID is required.
  *       500:
  *         description: Internal Server Error.
  */
