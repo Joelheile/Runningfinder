e2aecfd5437a002f165e6abcb117f7a7098fcd93
Joelheile <joel.heil-escobar@outlook.de>
2024-11-14 13:05:15 +0100

    🚚 Moved all files to git root folder and deleted scraper

--- scraper/InstagramScraper.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time, urllib.request
-import requests
-
-PATH = r"$HOME\Applications\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
-driver.get("https://www.instagram.com/")
