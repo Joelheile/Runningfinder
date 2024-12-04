244d82569ef6dc4dce850ccbb03724434419d038
Joelheile <joel.heil-escobar@outlook.de>
2024-12-04 11:59:13 +0100

    ✅ E2E test for creating and deleting club works

--- playwright-tests/clubRunFlow.spec.ts
+++ playwright-tests/clubRunFlow.spec.ts
@@ -1,20 +1,22 @@
 import { expect, test } from '@playwright/test';
 import path from 'path';
+import { v4 } from 'uuid';
 
 
 
 
 test('Test add club & run logic and delete both afterwards', async ({ page }) => {
+  const clubName= `${v4()} Club`
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: 'All Clubs 🏃' }).click();
   await page.getByRole('button', { name: 'Add Club' }).click();
 
   await page.getByLabel('Club Name').click();
-  await page.getByLabel('Club Name').fill('TestClub');
+  await page.getByLabel('Club Name').fill(clubName);
   await page.getByLabel('Club Name').press('Tab');
   await page.getByLabel('Club Description').fill('Test Description');
   await page.getByLabel('Club Description').press('Tab');
-  await page.getByLabel('Website URL (https://)').fill('https://clubwebsite.de');
+  await page.getByLabel('Website URL (https://)').fill('https://testwebsite.de');
   await page.getByLabel('Website URL (https://)').press('Tab');
   await page.getByLabel('Instagram Username').fill('TestUsername');
   const filePath = path.resolve(__dirname, "../public/assets/default-fallback-image.png");
@@ -22,9 +24,9 @@ test('Test add club & run logic and delete both afterwards', async ({ page }) =>
   await expect(page.locator('img[alt="Uploaded file"]')).toBeVisible();
 
   await page.getByRole('button', { name: 'Add Club' }).click();
+  await page.waitForTimeout(5000);
 
-
-  await page.getByRole('link', { name: 'TestClub ' }).click();
+  await page.getByRole('link', { name: clubName }).click();
 
   await page.getByRole('button', { name: 'Add first run 🏃‍♂️' }).click();
   await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').click();
@@ -42,11 +44,19 @@ test('Test add club & run logic and delete both afterwards', async ({ page }) =>
   await page.locator('div').filter({ hasText: /^Where are you starting\?$/ }).getByRole('textbox').fill('Starting Point');
   await page.getByRole('button', { name: 'Add run' }).click();
   await page.getByRole('button').first().click();
+  await page.waitForTimeout(5000);
+  await page.reload();
   await expect(page.getByText('FirstRun')).toBeVisible();
   await page.locator('div').filter({ hasText: /^FirstRun\|01:23\|48 km\|easy$/ }).getByRole('button').click();
-  await expect(page.getByText('Successfully registered for')).toBeVisible();
+  const height = await page.locator('div').filter({ hasText: /^FirstRun\|01:23\|48 km\|easy$/ }).evaluate(node => node.clientHeight);
+  if (height === 19) { // if the height is 19, the user is registered
+    await page.getByRole('button', { name: 'Register' }).click();
+  }
   await page.locator('div').filter({ hasText: /^FirstRun\|01:23\|48 km\|easy$/ }).getByRole('button').click();
-  await expect(page.getByText('Successfully deregistered')).toBeVisible();
+
+  if (height === 18) { // if the height is 19, the user is registered
+    await page.getByRole('button', { name: 'Register' }).click();
+  }
   await page.locator('div').filter({ hasText: /^Starting Point$/ }).getByRole('button').nth(1).click();
   await expect(page.getByText('Run deleted successfully')).toBeVisible();
   await expect(page.getByRole('button', { name: 'Add first run 🏃‍♂️' })).toBeVisible();
