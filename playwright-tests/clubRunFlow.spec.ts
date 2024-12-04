e25cc2142ec2d895b6c5bfddd623c295b0951bed
Joelheile <joel.heil-escobar@outlook.de>
2024-12-04 10:22:20 +0100

    🧪 Implemented test that does oauth manually, needs to be simplified

---
+++ playwright-tests/clubRunFlow.spec.ts
@@ -0,0 +1,68 @@
+import { expect, test } from '@playwright/test';
+
+
+
+test('Test add club & run logic and delete both afterwards', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'All Clubs 🏃' }).click();
+  await page.getByRole('button', { name: 'Add Club' }).click();
+  await page.getByRole('button', { name: 'Sign in with GitHub' }).click();
+  await page.getByLabel('Username or email address').fill('joels.tests@gmail.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill(process.env.NEXT_PUBLIC_GITHUB_TEST_PASSWORD as string);
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await page.goto('https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=AcMMx-dY0A7dX2AMtiPOFMNsogKN1yNLNBAIKtZuJZjUrwL1JXXnyH-BmJfTGwrHhTpWdDQ8-7Rqzg&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-1290724747%3A1733245020802148&ddm=1');
+  await page.getByLabel('Email or phone').fill('joels.tests@gmail.com');
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByLabel('Enter your password').fill(process.env.NEXT_PUBLIC_GMAIL_TEST_PASSWORD as string);
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.locator('tr.zA.zE[jscontroller="ZdOxDb"]').click();
+  const emailContent = await page.locator('div.a3s.aiL').innerText();
+  const verificationCodeMatch = emailContent.match(/Verification code: (\d{6})/);
+  const verificationCode = verificationCodeMatch ? verificationCodeMatch[1] : '';
+  console.log("Verification code: ", verificationCode);
+  await page.getByLabel('More message options').click();
+  await page.getByText('Delete this message').click();
+  await page.goto('http://localhost:3000/clubs/add');
+  await expect(page.getByRole('list')).toContainText('Verification code:');
+  await page.getByPlaceholder('XXXXXX').fill(verificationCode);
+  await page.getByLabel('Club Name').click();
+  await page.getByLabel('Club Name').fill('ClubName');
+  await page.getByLabel('Club Name').press('Tab');
+  await page.getByLabel('Club Description').fill('ClubDescription');
+  await page.getByLabel('Club Description').press('Tab');
+  await page.getByLabel('Website URL (https://)').fill('https://clubwebsite.de');
+  await page.getByLabel('Website URL (https://)').press('Tab');
+  await page.getByLabel('Instagram Username').fill('ClubUsername');
+  await page.getByRole('button', { name: 'Add Club' }).click();
+  await expect(page.getByRole('link', { name: 'ClubName ClubName' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'ClubName ClubName' }).click();
+
+  await page.getByRole('button', { name: 'Add first run 🏃‍♂️' }).click();
+  await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').click();
+  await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').fill('FirstRun');
+  await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').press('Tab');
+  await page.locator('div').filter({ hasText: /^DifficultyEasyIntermediateAdvanced$/ }).getByRole('combobox').press('Tab');
+  await page.locator('div').filter({ hasText: /^IntervalDailyWeeklyBiweeklyMonthly$/ }).getByRole('combobox').press('Tab');
+  await page.locator('div').filter({ hasText: /^WeekdayMondayTuesdayWednesdayThursdayFridaySaturdaySunday$/ }).getByRole('combobox').press('Tab');
+  await page.locator('input[type="time"]').press('Tab');
+  await page.locator('input[type="time"]').fill('01:23');
+  await page.getByRole('spinbutton').click();
+  await page.getByRole('spinbutton').fill('048');
+  await page.getByText('NameDifficultyEasyIntermediateAdvancedIntervalDailyWeeklyBiweeklyMonthlyWeekdayM').click();
+  await page.locator('div').filter({ hasText: /^Where are you starting\?$/ }).getByRole('textbox').click();
+  await page.locator('div').filter({ hasText: /^Where are you starting\?$/ }).getByRole('textbox').fill('Starting Point');
+  await page.getByRole('button', { name: 'Add run' }).click();
+  await page.getByRole('button').first().click();
+  await expect(page.getByText('FirstRun')).toBeVisible();
+  await page.locator('div').filter({ hasText: /^FirstRun\|01:23\|48 km\|easy$/ }).getByRole('button').click();
+  await expect(page.getByText('Successfully registered for')).toBeVisible();
+  await page.locator('div').filter({ hasText: /^FirstRun\|01:23\|48 km\|easy$/ }).getByRole('button').click();
+  await expect(page.getByText('Successfully deregistered')).toBeVisible();
+  await page.locator('div').filter({ hasText: /^Starting Point$/ }).getByRole('button').nth(1).click();
+  await expect(page.getByText('Run deleted successfully')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Add first run 🏃‍♂️' })).toBeVisible();
+  await page.getByRole('button').nth(2).click();
+  await expect(page.locator('div').filter({ hasText: 'Club deleted successfully' }).nth(2)).toBeVisible();
+})
