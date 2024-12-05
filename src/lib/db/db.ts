7a1e235d818349ba9e69860478c1d5dd384c898d
Joelheile <joel.heil-escobar@outlook.de>
2024-12-05 15:54:05 +0100

    🗃️ Added production and development databases

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,6 +1,25 @@
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
-import { drizzle } from "drizzle-orm/neon-http";
-config({ path: ".env.local" });
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL!);
-export const db = drizzle(sql);
+import { drizzle as neonDrizzle } from "drizzle-orm/neon-http";
+import { drizzle as localDrizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+
+  config({ path: ".env.local" });
+
+  const isTesting = process.env.NEXT_PUBLIC_TESTING === 'true';
+  const databaseUrl = isTesting ? process.env.NEXT_PUBLIC_DB_DEV : process.env.NEXT_PUBLIC_DB_PROD;
+
+  let db: any;
+
+  if (isTesting) {
+    const pool = new Pool({
+      connectionString: databaseUrl,
+    });
+    db = localDrizzle(pool);
+  } else {
+    const sql = neon(databaseUrl!);
+    db = neonDrizzle(sql);
+  }
+
+  export { db };
