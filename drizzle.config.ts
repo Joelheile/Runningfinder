ff13a5dc0a53a58e926a27b93ae0a9c1a0c655a8
Joelheile <joel.heil-escobar@outlook.de>
2024-12-08 21:14:58 +0100

    feat: connected the dev db successfully

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,13 +2,13 @@ import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 config({ path: ".env.local" });
-const isProdMode = process.env.NEXT_PUBLIC_TESTING === "false";
+const isTesting = process.env.NEXT_PUBLIC_TESTING === "true";
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/lib/db/schema",
   dbCredentials: {
-    url: isProdMode
-      ? process.env.NEXT_PUBLIC_DB_PROD!
-      : process.env.NEXT_PUBLIC_DB_DEV!,
+    url: isTesting
+      ? process.env.NEXT_PUBLIC_DB_DEV!
+      : process.env.NEXT_PUBLIC_DB_PROD!,
   },
 });
