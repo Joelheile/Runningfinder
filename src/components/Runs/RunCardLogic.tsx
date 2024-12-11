240b8c59fa900a5fcd6dc7bee2e1e2ffe2b58154
Joelheile <joel.heil-escobar@outlook.de>
2024-12-11 14:29:01 +0100

    chore: refactored functions into own components

--- src/components/Runs/RunCardLogic.tsx
+++ src/components/Runs/RunCardLogic.tsx
@@ -4,6 +4,9 @@ import { useDeleteRun } from "@/lib/hooks/runs/useDeleteRun";
 import { redirect } from "next/navigation";
 import { useState, useEffect } from "react";
 import RunCardUI from "./RunCardUI";
+import { Skeleton } from "@/components/UI/skeleton";
+import { getNextIntervalDate } from "@/lib/getNextIntervalDate";
+import { fetchWeatherData } from "@/lib/fetchWeatherData";
 
 interface RunCardProps {
   id: string;
@@ -17,32 +20,6 @@ interface RunCardProps {
   userId: string | undefined;
   slug: string;
 }
-
-const getNextIntervalDate = (intervalDay: number) => {
-  const today = new Date();
-  const nextDate = new Date();
-  nextDate.setDate(today.getDate() + ((intervalDay - today.getDay() + 7) % 7));
-  return nextDate;
-};
-
-const fetchWeatherData = async (lat: number, lng: number, date: Date) => {
-  const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&daily=temperature_2m_max,wind_speed_10m_max,uv_index_max&start_date=${date.toISOString().split("T")[0]}&end_date=${date.toISOString().split("T")[0]}&timezone=auto`
-  );
-  if (!response.ok) {
-    throw new Error(`Failed to fetch weather data: ${response.statusText}`);
-  }
-  const data = await response.json();
-  if (data && data.daily) {
-    return {
-      temperature: data.daily.temperature_2m_max[0],
-      wind: data.daily.wind_speed_10m_max[0],
-      uvIndex: data.daily.uv_index_max[0],
-    };
-  } else {
-    throw new Error("Invalid weather data");
-  }
-};
 
 export default function RunCard({
   id,
@@ -60,6 +37,7 @@ export default function RunCard({
   const [temperature, setTemperature] = useState(0);
   const [wind, setWind] = useState(0);
   const [uvIndex, setUvIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${location.lat},${location.lng}`;
 
@@ -91,11 +69,23 @@ export default function RunCard({
         setTemperature(weatherData.temperature);
         setWind(weatherData.wind);
         setUvIndex(weatherData.uvIndex);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Failed to fetch weather data:", error);
+        setLoading(false);
       });
   }, [intervalDay, location]);
+
+  if (loading) {
+    return (
+      <div className="mt-2">
+        <Skeleton className="h-6 w-1/4 mb-2" />
+        <Skeleton className="h-6 w-1/4 mb-2" />
+        <Skeleton className="h-6 w-1/4 mb-2" />
+      </div>
+    );
+  }
 
   return (
     <RunCardUI
