2f535a467091ff855b451a7c66d49d2d55181422
Joelheile <joel.heil-escobar@outlook.de>
2024-12-11 14:53:30 +0100

    fix: UI card improvements

--- src/components/Clubs/ClubCard.tsx
+++ src/components/Clubs/ClubCard.tsx
@@ -18,20 +18,25 @@ export default function ClubCard({
   websiteUrl,
 }: ClubCardProps) {
   return (
-    <div className="flex flex-col mt-5 sm:flex-row justify-center self-center w-auto max-w-2xl rounded-lg  min-h-44">
-      <div className="flex flex-col lg:flex-row bg-white gap-x-5 border p-3 rounded-md w-full">
-        <Image
-          width={500}
-          height={500}
-          src={avatarUrl || "/assets/default-fallback-image.png"}
-          alt={name}
-          className="rounded-md border w-full sm:w-1/3 lg:w-1/4 h-auto object-cover mb-4 sm:mb-0 sm:mr-6"
-        />
-        <div className="flex flex-col justify-between">
-          <div>
-            <h1 className="text-xl font-semibold">{name}</h1>
-            <p className="mt-2 text-gray-600">{description}</p>
-          </div>
+    <div className="flex flex-col mt-5 sm:flex-row justify-center self-center w-full max-w-2xl rounded-lg  min-h-44 bg-white border p-3">
+      <div className="flex-shrink-0 w-full sm:w-1/4">
+        <div className="relative w-full h-32 sm:h-full">
+          <Image
+            src={avatarUrl || "/assets/default-fallback-image.png"}
+            alt={name}
+            layout="fill"
+            className="rounded-md border object-cover "
+          />
+        </div>
+      </div>
+      <div className="flex flex-col justify-between ml-5 w-full sm:w-3/4 mt-4 sm:mt-0">
+        <div className="flex-grow overflow-hidden">
+          <h1 className="text-xl font-semibold truncate">{name}</h1>
+          <p className="mt-2 text-gray-600 overflow-hidden text-ellipsis line-clamp-3">
+            {description}
+          </p>
+        </div>
+        <div className="flex-shrink-0 mt-4 sm:mt-0">
           <ClubIconBar
             instagramUsername={instagramUsername}
             websiteUrl={websiteUrl}
