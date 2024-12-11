5c61c832dc0e2335a42a1c11a9687824d92abde8
Joelheile <joel.heil-escobar@outlook.de>
2024-12-11 11:57:22 +0100

    fix: changed layout from top to right

--- src/components/Clubs/SelectedClubHeaderUI.tsx
+++ src/components/Clubs/SelectedClubHeaderUI.tsx
@@ -3,6 +3,7 @@ import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import ClubIconBar from "../Icons/ClubIconBar";
+import { Button } from "../UI/button";
 
 interface SelectedClubHeaderUIProps {
   club: Club;
@@ -18,36 +19,35 @@ export default function SelectedClubHeaderUI({
   setInstagramSelected,
 }: SelectedClubHeaderUIProps) {
   return (
-    <div className="bg-white/80 backdrop-blur-sm absolute top-0 left-0 z-10 w-full text-card-foreground shadow-sm lg:p-6 sm:p-4 space-y-4">
-      <Link href={`/clubs/${club.slug}`}>
-        <div className="flex flex-col sm:flex-row justify-between items-start">
-          <div className="flex flex-col sm:flex-row w-full">
-            <Image
-              width={500}
-              height={500}
-              src={avatarUrl}
-              alt={club.name}
-              className="rounded-md border w-auto max-w-48 sm:w-1/6 h-auto max-h-48 object-cover mb-4 sm:mb-0 sm:mr-6"
-            />
-            <div className="flex flex-col justify-between w-full sm:w-2/3">
-              <div>
-                <h2 className="text-lg sm:text-xl font-semibold">
-                  {club.name}
-                </h2>
-                <p className="lg:w-1/3 mt-2">{club.description}</p>
+    <div className="bg-white/80 backdrop-blur-sm absolute top-0 right-0 z-10 h-5/6 w-1/6 text-card-foreground shadow-sm lg:p-6 sm:p-4 space-y-4 flex flex-col justify-between">
+      <div className="flex flex-col justify-between items-center h-full">
+        <div className="flex flex-col items-center w-full">
+          <Image
+            width={500}
+            height={500}
+            src={avatarUrl}
+            alt={club.name}
+            className="rounded-md border w-auto max-w-48 h-auto max-h-48 object-cover mb-4"
+          />
+          <div className="flex flex-col justify-between mx-auto w-full text-center">
+            <div>
+              <h2 className="text-lg sm:text-xl font-semibold">{club.name}</h2>
+              <p className="mt-2">{club.description}</p>
+              <div className="flex justify-center">
+                <ClubIconBar
+                  instagramUsername={club.instagramUsername || ""}
+                  websiteUrl={club.websiteUrl || ""}
+                />
               </div>
-              <ClubIconBar
-                instagramUsername={club.instagramUsername || ""}
-                websiteUrl={club.websiteUrl || ""}
-              />
             </div>
           </div>
-          <div className="flex md:self-center mt-4">
-            <strong className="text-primary"> Club</strong>
-            <ChevronRight className="stroke-primary" />
-          </div>
         </div>
-      </Link>
+        <div className=" flex-col mt-4">
+          <Link href={`/clubs/${club.slug}`}>
+            <Button>Go to runs </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
