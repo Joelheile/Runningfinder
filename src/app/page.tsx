b727f08d100dc2cfd53038c029299ed40cc277b4
Joelheile <joel.heil-escobar@outlook.de>
2025-01-12 22:31:16 +0100

    fixed es lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default async function HomePage() {
 
   return (
     <div>
-      //TODO: Add like button
       <div className="absolute z-10 top-2 right-1/2 left-1/2   text-center">
         <Link href="/clubs">
           <Button variant={"outline"}>All Clubs 🏃</Button>
