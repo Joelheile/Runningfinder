8baa64fa37a12d74637e22c74e7f909e5a1cbc44
Joelheile <joel.heil-escobar@outlook.de>
2025-01-12 23:48:36 +0100

    feat: added fetching runs

--- src/components/Runs/UserRuns.tsx
+++ src/components/Runs/UserRuns.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useFetchRegistrationByUser } from "@/lib/hooks/registrations/useFetchUserRegistrations";
+import { useFetchRuns } from "@/lib/hooks/runs/useFetchRuns";
+import { Run } from "@/lib/types/Run";
 
 type UserRunsProps = {
   userId: string;
@@ -13,6 +15,23 @@ export default function UserRuns({ userId }: UserRunsProps) {
     error: registrationsError,
   } = useFetchRegistrationByUser(userId);
   console.log("registrations", registrations);
+
+  const {
+    data: runs,
+    isLoading: runsLoading,
+    error: runError,
+  } = useFetchRuns({});
+
+  const userRuns: (Run | undefined)[] = [];
+
+  if (registrations && Array.isArray(registrations)) {
+    registrations.forEach((registration) => {
+      const run = runs?.find((run: any) => run.id === registration.runId);
+      userRuns.push(run);
+    });
+  }
+  console.log("userRuns", userRuns);
+
   return (
     <div>
       <h1>My Runs</h1>
