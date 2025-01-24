e1549609ba3ba9fbc921f34b2d4858454f45ac00
Joelheile <joel.heil-escobar@outlook.de>
2025-01-24 15:41:09 +0100

    fix: implemented working fetch

--- src/lib/hooks/runs/scrapeRuns.ts
+++ src/lib/hooks/runs/scrapeRuns.ts
@@ -22,47 +22,61 @@ export async function scrapeRuns() {
 
     const clubs = await fetch("/api/clubs/").then((r) => r.json());
     console.log("clubs", clubs);
+    if (clubs != null){
 
-    const clubNames = clubs.map((club: any) => club.name).join(", ");
-    const message = `Imagine you are a comparison bot and your only task is to compare if an input string can be found inside of an array. This is the input (a running club): ${run.clubName}. It has to be very similar, but if for example Berlin is appended, and you can find it in the array, it is the same. Output only true or false.
-        Array: ${clubNames}`;
-
-    const sendMessage = await fetch("/api/ai", {
-      method: "POST",
-      body: JSON.stringify({ message }),
-    }).then((r) => r.json());
-
-    if (message === "true") {
-      console.log("club already exists", run.clubName);
-    } else {
-      // call instagram api / scrape it
-      const instagramData = await fetch(`https://api.apify.com/v2/acts/apify~instagram-profile-scraper/run-sync-get-dataset-items?token=${process.env.NEXT_PUBLIC_APIFY_KEY}`, {
+      const clubNames = clubs.map((club: any) => club.name).join(", ");
+      const message = `Imagine you are a comparison bot and your only task is to compare if an input string can be found inside of an array. This is the input (a running club): ${run.clubName}. It has to be very similar, but if for example Berlin is appended, and you can find it in the array, it is the same. Output only true or false.
+          Array: ${clubNames}`;
+  
+      const sendMessage = await fetch("/api/ai", {
         method: "POST",
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({ usernames: ["optimisticrunners"] }),
+        body: JSON.stringify({ message }),
       }).then((r) => r.json());
-      console.log("instagramData", instagramData);
-
-      if (instagramData.items.length > 0) {
-        const instagramUsername = instagramData.items[0].username;
-
-        const addAvatar
-      }
-      
-      const addclubs = await fetch("/api/clubs", {
+  
+      if (message === "true") {
+        console.log("club already exists", run.clubName);
+      } else {
+        // call instagram api / scrape it
+        // const instagramData = await fetch(`https://api.apify.com/v2/acts/apify~instagram-profile-scraper/run-sync-get-dataset-items?token=${process.env.NEXT_PUBLIC_APIFY_KEY}`, {
+        //   method: "POST",
+        //   headers: {'Content-Type':'application/json'},
+        //   body: JSON.stringify({ usernames: ["optimisticrunners"] }),
+        // }).then((r) => r.json());
+        // console.log("instagramData", instagramData);
+  
+        // if (instagramData.items.length > 0) {
+  
+        //   const avatarFile = await fetch(instagramData.items[0].profilePicUrl).then((r) =>
+        //     r.blob()
+        //   );
+  
+        //   const uploadAvatar = useUploadAvatar();
+        //   const convertedFile = new File([avatarFile], "avatar.jpg", { type: avatarFile.type });
+  
+        //   const avatarFileId = await uploadAvatar.uploadAvatar(convertedFile, v4());
+        //   console.log("avatarFileId", avatarFileId);
+        // }
+        const addclubs = await fetch("/api/clubs", {
           method: "POST",
           body: JSON.stringify({
               id: v4(),
-              name: "",
-              location: { lat: 0, lng: 0 },
+              name: run.name,
+              location: { lat: run.lat, lng: run.lng },
               description: "",
-              instagramUsername: "",
-              websiteUrl: "",
-              stravaUsername: "",
+              instagramUsername: run.instagramUsername,
+              websiteUrl: run.locationLink,
+              stravaUsername: run.stravaUsername,
               avatarFileId: "",
             }),
           }).then((r) => r.json());
         }
+      
+    }
+
+
+      
+      
+    
         
         // check if run is already in the database (filer)
         const runs = await fetch("/api/runs/").then((r) => r.json());
