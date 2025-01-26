a442a972286fd903eeccce32ad588ab3d95793e5
Joelheile <joel.heil-escobar@outlook.de>
2025-01-26 14:33:22 +0100

    fix: updated filter bar styling

--- src/components/Runs/FilterBarLogic.tsx
+++ src/components/Runs/FilterBarLogic.tsx
@@ -5,19 +5,14 @@ import { useEffect, useState } from "react";
 import FilterBarUI from "./FilterBarUI";
 
 interface FilterBarLogicProps {
-  onFilterChange: (filters: {
-    maxDistance?: number;
-    days?: number[];
-    difficulty?: string;
-  }) => void;
+  onFilterChange: (filters: { days?: number[] }) => void;
 }
 
 export default function FilterBar({ onFilterChange }: FilterBarLogicProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const allowedDistances = [5, 7, 10, 15, 21, 42];
-  const [distanceIndex, setDistanceIndex] = useState<number | null>(null);
   const [selectedDays, setSelectedDays] = useState<number[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const [difficulty, setDifficulty] = useState<string | null>(null);
 
   const toggleDay = (dayValue: number) => {
@@ -29,60 +24,40 @@ export default function FilterBar({ onFilterChange }: FilterBarLogicProps) {
   };
 
   const resetFilters = () => {
-    setDistanceIndex(null);
     setSelectedDays([]);
-    setDifficulty(null);
   };
 
   useEffect(() => {
-    const filters = {
-      maxDistance:
-        distanceIndex !== null ? allowedDistances[distanceIndex] : undefined,
+    const filters: { days?: number[] } = {
       days: selectedDays.length > 0 ? selectedDays : undefined,
-      difficulty: difficulty || undefined,
     };
 
     onFilterChange(filters);
 
     const query = new URLSearchParams();
     Object.entries(filters).forEach(([key, value]) => {
-      if (value !== undefined) {
-        if (Array.isArray(value)) {
-          query.set(key, value.join(","));
-        } else {
-          query.set(key, value.toString());
-        }
+      if (value !== undefined && Array.isArray(value)) {
+        query.set(key, value.join(","));
       }
     });
 
     router.push(`?${query.toString()}`);
-  }, [distanceIndex, selectedDays, difficulty]);
+  }, [selectedDays]);
 
   useEffect(() => {
-    const maxDistance = searchParams.get("maxDistance");
     const weekday = searchParams.get("weekday");
-    const difficulty = searchParams.get("difficulty");
-
-    if (maxDistance) {
-      const maxDistanceIndex = allowedDistances.indexOf(
-        parseInt(maxDistance, 10)
-      );
-      if (maxDistanceIndex !== -1) setDistanceIndex(maxDistanceIndex);
-    }
-
     if (weekday) setSelectedDays(weekday.split(",").map(Number));
-    if (difficulty) setDifficulty(difficulty);
   }, [searchParams]);
 
   return (
     <FilterBarUI
-      allowedDistances={allowedDistances}
-      distanceIndex={distanceIndex}
       selectedDays={selectedDays}
-      toggleDay={toggleDay}
-      setDistanceIndex={setDistanceIndex}
+      setSelectedDays={setSelectedDays}
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
       difficulty={difficulty}
       setDifficulty={setDifficulty}
+      toggleDay={toggleDay}
       resetFilters={resetFilters}
     />
   );
